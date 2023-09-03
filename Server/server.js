5ce401f4df498e671cfedebf3974ae94f850f9d7
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-09-03 23:03:47 +0100

    added a cor origin url to the cors()

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(morgan('dev')); // log details to the console for every request made to the browser;
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://movie-database-api.onrender.com'],
+    origin: ['http://localhost:3000', 'https://flicksondemand.netlify.app'],
     credentials: true,
   })
 ); // allows requests from all origins
