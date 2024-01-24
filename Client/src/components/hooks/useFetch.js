521ebda6cbbb3852f88e5a72c1b0b42d377d5553
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2024-01-24 01:58:29 +0100

    removed unecesary code and implemented use of React-Query

--- Client/src/components/hooks/useFetch.js
+++
@@ -1,8 +0,0 @@
-export const useFetch = async function (endpoint) {
-  try {
-    const res = await fetch(endpoint);
-    return await res.json();
-  } catch (err) {
-    console.error(err);
-  }
-};
